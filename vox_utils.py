454e29bbbb6f9676d204f6de9c81c0f90c7ba2e1
Flurin Gishamer <fgishamer@me.com>
2018-11-03 14:11:01 +0100

    minor

--- vox_utils.py
+++ vox_utils.py
@@ -1,4 +1,6 @@
+import audio_tools
 
+# ToDo: write methods that gets spectrogram from voxceleb, and compiles them into train and test sets
 def get_data() -> tuple:
     """
     :return: tuple containing traind and test sets as ndarray
