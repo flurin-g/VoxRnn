9944694a6a64bc710ed1898d0c7407e8532013f1
Philippe Hürlimann <p@hurlimann.org>
2018-11-12 15:01:17 +0100

    quick hack to hopefuly fix the ram issue

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,7 @@
 import itertools
 import math
 import random
+import threading
 
 import pandas as pd
 
@@ -11,6 +12,8 @@ class DataGenerator(ks.utils.Sequence):
     'Generates data for Keras'
 
     def __init__(self, dataset: pd.DataFrame, dim: tuple, batch_size: int, shuffle: bool):
+        self.d_lock = threading.Lock()
+        self.spectrograms = dict()
         self.dim = dim
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -34,8 +37,8 @@ class DataGenerator(ks.utils.Sequence):
         for i, pair in enumerate(batch_indices):
             row_a = self.dataset.loc[pair[0]]
             row_b = self.dataset.loc[pair[1]]
-            X[current_batch][0:self.dim[0]] = np.load(row_a['spectrogram_path'])
-            X[current_batch][self.dim[0]:2 * self.dim[0]] = np.load(row_a['spectrogram_path'])
+            X[current_batch][0:self.dim[0]] = load_or_get_spectrogram(row_a['spectrogram_path'])
+            X[current_batch][self.dim[0]:2 * self.dim[0]] = load_or_get_spectrogram(row_a['spectrogram_path'])
 
             if row_a['speaker_id'] == row_b['speaker_id']:
                 y[i] = 1
@@ -47,3 +50,12 @@ class DataGenerator(ks.utils.Sequence):
     def on_epoch_end(self):
         if self.mode == 'shuffle':
             random.shuffle(self.pair_indices)
+
+    def load_or_get_spectrogram(self, filepath):
+        """ TODO: fix this ugly hack """
+        self.d_lock.acquire()
+        if filepath not in self.spectrograms:
+            self.spectrograms[filepath] = np.load(filepath)
+        self.d_lock.release()
+
+        return self.spectrograms[filepath]
