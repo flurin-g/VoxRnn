319257c9e283592d25295e43f2cc4bd71e2cc699
Flurin Gishamer <fgishamer@me.com>
2018-12-12 23:08:26 +0100

    +option for pre-train

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import argparse
 
-from vox_rnn_model import train_model
+from vox_rnn_model import train_model, pre_train_model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -9,7 +9,11 @@ if __name__ == '__main__':
     parser.add_argument('-S', '--create-spectrograms', dest='create_spectrograms', default=False,
                         action='store_true')
 
+    parser.add_argument('-M', '--mode', type=str, dest='training_mode', default="train")
+
     args = parser.parse_args()
 
-    train_model(
-        create_spectrograms=args.create_spectrograms)
+    if args.training_mode == "pre-train":
+        pre_train_model(create_spectrograms=args.create_spectrograms)
+    else:
+        train_model(create_spectrograms=args.create_spectrograms)
