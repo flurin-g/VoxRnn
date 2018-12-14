78c28ae5759ef31c8905987de7437ef708045c0e
Flurin Gishamer <fgishamer@me.com>
2018-12-14 11:30:45 +0100

    +histogram freq = 0

--- vox_rnn_model.py
+++ vox_rnn_model.py
@@ -147,7 +147,7 @@ def train_model(create_spectrograms: bool = False, weights_path: str = WEIGHTS_P
         ks.callbacks.ModelCheckpoint(checkpoint_pattern),
         ks.callbacks.TensorBoard(
             LOG_DIR,
-            histogram_freq=1,
+            histogram_freq=0,
             write_grads=True,
             write_images=True,
             write_graph=True
